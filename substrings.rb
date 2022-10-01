903e9cd026a72683b18973982efe4ffc94b123b1
tracyneproski <tracyneproski@gmail.com>
2022-10-01 10:37:02 -0500

    Add pseudocode outline to project

--- substrings.rb
+++ substrings.rb
@@ -9,3 +9,23 @@ substrings("below", dictionary)
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+def substrings(word, word_array)
+  for each object in word_array
+    if object.include? word #or somehow subset of word
+      push to hash
+    else
+      go to next
+    end
+    return hash #maybe here
+  end
+  return hash #or here
+end
+
+def full_sentence(string, word_array)
+  split string into parts
+  run substring for each part
+  return hash
+end
+
+
+
