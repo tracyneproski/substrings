f3c95174d79e323397f2064e1a60f94e2002518c
tracyneproski <tracyneproski@gmail.com>
2022-10-01 10:36:27 -0500

    Add pseudocode outline to project

---
+++ substrings.rb
@@ -0,0 +1,11 @@
+#Odin Project - Substrings
+'''
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("below", dictionary)
+=> { "below" => 1, "low" => 1 }
+'''
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
